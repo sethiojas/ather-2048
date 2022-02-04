1630e37005a121e0014b73fa421dff9ed937980c
Ojas Sethi <sethi.ojas@gmail.com>
2022-02-04 22:56:28 +0530

    added Game class

--- main.py
+++ main.py
@@ -18,15 +18,16 @@
 
 import random # used to randomize appearance of new tiles and tile value (i.e 2 or 4)
 
-2. User will input 1, 2, 3, 4 for left, right, up and down movements
-
-3. Program will then merge all the tiles in given direction and show the latest sums
-according to rules mentioned above
-
-4. Next it should select a random empty location in tiles and place a 2 or a 4
-
-5. Repeat steps 1 - 4 till one of the cell reaches 2048
-'''
-
+class Game():
+    board_size = 4
+    game_max_value = 2048
+    play_instructions = '''Play the game via following commands,
+    Enter:
+    1 - To move tiles Left.
+    2 - To move tiles Right.
+    3 - To move tiles Up.
+    4 - To move tiles Down.
+    '''
+    
 import random # used to randomize appearance of new tiles and tile value (i.e 2 or 4)
 
