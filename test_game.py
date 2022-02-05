e97a5f186aca19b46af1ed32fd25d14f58d016a7
Ojas Sethi <sethi.ojas@gmail.com>
2022-02-05 13:17:55 +0530

    Rename main.py to game.py
    .

--- test_game.py
+++ test_game.py
@@ -1,14 +1,14 @@
 # Unit tests for main.py
 
 import unittest
-import main
+import game
 
 class TestGame(unittest.TestCase):
     def test_static_variables(self) -> None:
         '''Test static variables of Game class'''
         
-        self.assertEqual(main.Game.board_size, 4, "Board Size does not match")
-        self.assertEqual(main.Game.game_max_value, 2048, "Game max value does not match")
+        self.assertEqual(game.Game.board_size, 4, "Board Size does not match")
+        self.assertEqual(game.Game.game_max_value, 2048, "Game max value does not match")
 
         # Do not change the indentation inside multi-line string
         # as it will interpret it as space in actual string, which
@@ -20,4 +20,4 @@ class TestGame(unittest.TestCase):
     3 - To move tiles Up.
     4 - To move tiles Down.
     '''
-        self.assertEqual(main.Game.play_instructions, play_instruction, "Play instructions do not match")
+        self.assertEqual(game.Game.play_instructions, play_instruction, "Play instructions do not match")
